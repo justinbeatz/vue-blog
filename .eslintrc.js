b89dc42c3cb3136ad89cd1566c108346d3b705d6
Andres Ortega <andrew.ortega@gmail.com>
2018-10-08 17:59:04 -0400

    Added login files, bootstrap 4, json-server

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,9 @@ module.exports = {
       optionalDependencies: ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // "indent": ["error", 4],
+    // "vue/html-indent": ["error", 4],
+    // "vue/script-indent": ["error", 4],
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App';
 import router from './router';
+// setup fake backend
+import { configureFakeBackend } from './helpers/export-star';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+
+configureFakeBackend();
 
 /* eslint-disable no-new */
 new Vue({
